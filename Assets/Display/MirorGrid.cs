b39a4ee51465559ded20d833b25c577f79a8a620
roulios44 <jules.croizier@gmail.com>
2022-09-23 19:23:24 +0200

    all pieces set and back to transparent grid

--- Assets/Display/MirorGrid.cs
+++ Assets/Display/MirorGrid.cs
@@ -8,14 +8,14 @@ public class MirorGrid{
     private _GridDisplay _grid = GameObject.FindObjectOfType<_GridDisplay>();
     private int height = 0;
     private int widht = 0;
-    private Piece test = new IPiece();
+    private Piece test = new JPiece();
     public void SetGridBackground(){
         height = _grid.height;
         widht = _grid.width;
         for (int i=0;i<_grid.height;i++){
             List<SquareColor> LigneColor = new List<SquareColor>();
             for (int j=0;j<_grid.width;j++){
-                SquareColor color = SquareColor.GREEN;
+                SquareColor color = SquareColor.TRANSPARENT;
                 LigneColor.Add(color);
             }
             mirorGrid.Add(LigneColor);   
@@ -25,7 +25,7 @@ public class MirorGrid{
     public void GeneratePiece(){
         for (int i = 0; i<test.ListX.Count;i++){
             if (test.ListY[i] < 21){
-                mirorGrid[test.ListY[i]][test.ListX[i]] = SquareColor.GREEN;
+                mirorGrid[test.ListY[i]][test.ListX[i]] = SquareColor.TRANSPARENT;
                 test.ChangeListY(i,test.ListY[i]+1);
                 mirorGrid[test.ListY[i]][test.ListX[i]] = test.colorPiece;
                 }
--- Assets/Display/Piece.cs
+++ Assets/Display/Piece.cs
@@ -11,12 +11,58 @@ public class Piece{
     public void ChangeListY(int index, int value){
         ListY[index] = value;
     }
+    //TODO add a metod to watch piece under each pixel 
 }
+
+
+//Part with all pieces specifiquations
 
 public class IPiece : Piece{
     public IPiece() : base(){
         ListX = new List<int>(new int[] {5,6,7,8});
         ListY = new List<int> (new int[] {0,0,0,0});
         colorPiece = SquareColor.LIGHT_BLUE;
+    }
+}
+public class TPiece : Piece{
+    public TPiece() : base(){
+        ListX = new List<int>(new int[] {5,6,6,7});
+        ListY = new List<int>(new int[] {1,1,0,1});
+        colorPiece = SquareColor.PURPLE;
+    }
+}
+public class OPiece : Piece{
+    public OPiece() : base(){
+        ListX = new List<int>(new int[] {5,6,5,6});
+        ListY = new List<int>(new int[] {0,0,1,1});
+        colorPiece = SquareColor.YELLOW;
+    }
+}
+public class ZPiece : Piece{
+    public ZPiece() : base(){
+        ListX = new List<int>(new int[] {5,6,6,7});
+        ListY = new List<int>(new int[] {0,0,1,1});
+        colorPiece = SquareColor.RED;
+    }
+}
+public class SPiece : Piece{
+    public SPiece() : base(){
+        ListX = new List<int>(new int[] {5,6,6,7});
+        ListY = new List<int>(new int[] {1,1,0,0});
+        colorPiece = SquareColor.GREEN;
+    }
+}
+public class LPiece : Piece{
+    public LPiece() : base(){
+        ListX = new List<int>(new int[] {5,6,7,7});
+        ListY = new List<int>(new int[] {0,0,0,1});
+        colorPiece = SquareColor.ORANGE;
+    }
+}
+public class JPiece : Piece{
+    public JPiece() : base(){
+        ListX = new List<int>(new int[] {5,5,6,7});
+        ListY = new List<int>(new int[] {1,0,0,0});
+        colorPiece = SquareColor.DEEP_BLUE;
     }
 }
